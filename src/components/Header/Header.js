4fd846a1b2ae3bb5b4d04398d7469cc316a414a9
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-11-25 11:47:39 +0100

    Add 17.2 TASK: Development of sub-pages - Icon in Header taken from dataStore.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,17 +3,19 @@ import { NavLink, Link } from 'react-router-dom';
 import Container from '../Container/Container';
 import Icon from '../Icon/Icon';
 import styles from './Header.scss';
+import { settings } from '../../data/dataStore';
 
 class Header extends React.Component {
 
   render() {
+    const {icon} = settings.headerIcon;
    
     return (
       <header className={styles.component}>
         <Container>
           <div className={styles.wrapper}>
             <Link to='/' className={styles.logo}>
-              <Icon name='paw' />
+              <Icon name={icon} />
             </Link>
             <nav>
               <NavLink exact to='/' activeClassName='active'>Home</NavLink>
