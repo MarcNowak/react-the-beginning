c15b613d86de84fd2e5b0bf46998ccdef7d8634c
Marcin Nowak <maarcin.nowaak@gmail.com>
2021-11-28 19:09:22 +0100

    Add 17.4 TASK: cards to SearchResults dispatching - in progress v3

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -8,7 +8,7 @@ import { settings } from '../../data/dataStore';
 
 class Search extends React.Component {
   static propTypes = {
-    history: PropTypes.node,
+    history: PropTypes.any,
   }
 
   state = {
@@ -38,7 +38,7 @@ class Search extends React.Component {
           onChange={event => this.handleChange(event)}
         />
         <div className={styles.buttons}>
-          <Button onClick={this.handleOK}><Icon name={icon} /></Button>
+          <Button onClick={() => this.handleOK()}><Icon name={icon} /></Button>
         </div>
       </div>
     );
